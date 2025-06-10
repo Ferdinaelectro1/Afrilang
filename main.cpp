ec728c322ac6738a5d54bb39c357855f36748c6d
Ferdinand <atiferdinand31@gmail.com>
2025-06-10 23:11:30 +0100

    support d'un fichier .afr pouvant être exécuter.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
-#include "lexer2.h"
-#include "Parser2.h"
-#include "Token.h"
+#include "src/lexer2.h"
+#include "src/Parser2.h"
+#include "src/Token.h"
+#include "src/run_file.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
     std::string code;
     Parser2 parser;
-    system("clear");
-    std::cout << "" << std::endl;
-    std::cout <<"Bienvenue dans Afrilang. Veuillez saisir vos lignes de code."<<std::endl;
-    std::cout << "" << std::endl;
-    while (code != "quit")
+    if(argc == 1)
     {
-        std::cout << ">> ";
-        std::getline(std::cin,code);
-        Lexer2 lexer(code);
-        parser.parse2(lexer.generate_tokens());
+        system("clear");
+        std::cout << "" << std::endl;
+        std::cout <<"Bienvenue dans Afrilang. Veuillez saisir vos lignes de code."<<std::endl;
+        std::cout << "" << std::endl;
+        while (code != "quit")
+        {
+            std::cout << ">> ";
+            std::getline(std::cin,code);
+            Lexer2 lexer(code);
+            parser.parse2(lexer.generate_tokens());
+        }
+    }
+    else if(argc == 2)
+    {
+        run_file(argv[1]);
     }  
+    else
+    {
+        std::cerr << "Format d'entrée : './Afrilang' ou './Afrilang fileName'"<<std::endl;
+    }
 /*     std::vector<Token> tokens;
     Lexer2 lexer("af f ich er(\"Mon ie ur\") et toi");
     tokens = lexer.generate_tokens();   */
---
+++ src/run_file.h
@@ -0,0 +1,37 @@
+#ifndef __RUN_FILE_H__
+#define __RUN_FILE_H__
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include "lexer2.h"
+#include "Parser2.h"
+#include "Token.h"
+
+void run_file(const std::string file_name)
+{
+    const int file_size = file_name.size();
+    if(!(file_name[file_size - 1] == 'r' && file_name[file_size - 2] == 'f' && file_name[file_size - 3] == 'a' && file_name[file_size - 4] == '.'))
+    {
+        std::cerr << "Erreur : Fichier non reconnu. L'extention doit être '.afr'"<< std::endl;
+        return;
+    }
+    std::ifstream file;
+    file.open(file_name);
+    if(!file)
+    {
+        std::cerr << "Erreur lors de l'ouverture du fichier : '"<<file_name<<"'"<<std::endl;
+        return;
+    }
+
+    std::string line_code = "";
+    Parser2 parser;
+    while (std::getline(file,line_code))
+    {
+        
+        Lexer2 lexer(line_code);
+        parser.parse2(lexer.generate_tokens()); 
+    }
+    file.close();
+}
+
+#endif
